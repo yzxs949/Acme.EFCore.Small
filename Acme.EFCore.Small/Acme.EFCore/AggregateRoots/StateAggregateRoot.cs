c4bfcff51c54f8fa154d25a4a0926f0de3d9620d
云中小生 <yzxs949@163.com>
2024-12-19 11:12:14 +0800

    更新状态聚合根

--- Acme.EFCore.Small/Acme.EFCore/AggregateRoots/StateAggregateRoot.cs
+++ Acme.EFCore.Small/Acme.EFCore/AggregateRoots/StateAggregateRoot.cs
@@ -1,9 +1,11 @@
-﻿namespace Acme.EFCore.AggregateRoots;
+﻿using System;
+
+namespace Acme.EFCore.AggregateRoots;
 
 /// <summary>
 /// 状态聚合根
 /// </summary>
-/// <typeparam name="TKey"></typeparam>
+/// <typeparam name="TKey">主键类型</typeparam>
 /// <remarks>
 /// 构造函数，初始化状态聚合根
 /// </remarks>
@@ -13,7 +15,11 @@ public abstract class StateAggregateRoot<TKey> : IdAggregateRoot<TKey> where TKe
     /// 构造函数，初始化状态聚合根
     /// </summary>
     /// <param name="isDisable"></param>
-    public StateAggregateRoot(bool isDisable) => IsDisable = isDisable;
+    public StateAggregateRoot(bool isDisable)
+    {
+        IsDisable = isDisable;
+        OperationTime = DateTime.Now;
+    }
 
     /// <summary>
     /// 是否禁用
@@ -21,18 +27,32 @@ public abstract class StateAggregateRoot<TKey> : IdAggregateRoot<TKey> where TKe
     public bool IsDisable { get; private set; }
 
     /// <summary>
+    /// 操作人Id
+    /// </summary>
+    public TKey OperatorId { get; private set; }
+
+    /// <summary>
+    /// 操作时间
+    /// </summary>
+    public DateTime? OperationTime { get; private set; }
+
+    /// <summary>
     /// 启用
     /// </summary>
-    public void Enable()
+    public void Enable(TKey operatorId)
     {
         IsDisable = false;
+        OperatorId = operatorId;
+        OperationTime = DateTime.Now;
     }
 
     /// <summary>
     /// 禁用
     /// </summary>
-    public void Disable()
+    public void Disable(TKey operatorId)
     {
         IsDisable = true;
+        OperatorId = operatorId;
+        OperationTime = DateTime.Now;
     }
 }
